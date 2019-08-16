90f0ed4579a4859390a3262325cb97ee315176d3
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-16 10:47:47 +0200

    Cotizar seguro, calculo de la cantidad a pagar

--- js/app.js
+++ js/app.js
@@ -16,6 +16,49 @@ function Seguro(marca, anio, tipo){
     this.tipo = tipo;
 }
 
+Seguro.prototype.cotizarSeguro = function(){
+   /*
+        1 = americano 1.15
+        2 = asiatico 1.05
+        3 = europeo 1.35
+   */
+
+   let cantidad;
+   const base = 2000;
+   switch (this.marca) {
+       case '1':
+           cantidad = base * 1.15;
+           break;
+       case '2':
+           cantidad = base * 1.05;
+           break;
+       case '3':
+           cantidad = base * 1.35;
+           break;
+   }
+
+   //Calcular años a cotizar
+   const diferencia = new Date().getFullYear() - this.anio;
+
+   //Cada año de diferencia hay que reducir 3% el valor del seguro
+   cantidad -= ((diferencia * 3) * cantidad) / 100;
+
+   /*
+        Si el seguro es básico se incrementa 30% 
+        Si el seguro es clompleto se incrementa 50% 
+   */
+   
+   if(this.tipo === 'basico'){
+       cantidad *= 1.30;
+   }else{
+    cantidad *= 1.50;
+   }
+
+   console.log(cantidad);
+
+   return cantidad;
+}
+
 //Todo lo que muestra la interfaz
 function Interfaz() {}
 
@@ -57,8 +100,14 @@ formulario.addEventListener('submit', function(e){
 
     // Revisamos que los campos no esten vacios
     if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === ''){
+        //Interfaz imprimiendo mensaje de error
         interfaz.mostrarMensaje('Faltan datos, revisa el formulario y prueba de nuevo', 'error');
     }else{
-        console.log('Todo correcto');
+        //Instanciar seguro y mostrar interfaz
+        const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo);
+        //console.log(seguro);
+
+        //Cotizar el seguro        
+        const cantidad = seguro.cotizarSeguro();
     }
 });
