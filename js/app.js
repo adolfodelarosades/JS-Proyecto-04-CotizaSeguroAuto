9ce5dd7ca34bb71a4c8dbb5ba04b94dc29bb04c1
Adolfo <adolfodelarosa2012@gmail.com>
2019-08-16 09:53:56 +0200

    Recuperar datos del formulario

--- js/app.js
+++ js/app.js
@@ -8,3 +8,43 @@ for( let i = max; i > min; i--){
     option.innerHTML = i;
     selectAnios.appendChild(option);
 }
+
+//Constructor para seguros
+function Seguro(marca, anio, tipo){
+    this.marca = marca;
+    this.anio = anio;
+    this.tipo = tipo;
+}
+
+//Todo lo que muestra la interfaz
+function Interfaz() {
+
+}
+
+//EventListener
+const formulario = document.getElementById('cotizar-seguro');
+
+formulario.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    // Leer marca seleccionada del select
+    const marca = document.getElementById('marca');
+    const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+
+    // Leer el año seleccionado del <select>
+    const anio = document.getElementById('anio');
+    const anioSeleccionado = anio.options[anio.selectedIndex].value;
+
+    // Leer el valor del radio button
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    //Crear instancia de Interfaz
+    const interfaz = new Interfaz();
+
+    // Revisamos que los campos no esten vacios
+    if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === ''){
+        console.log('Faltan Datos');
+    }else{
+        console.log('Todo correcto');
+    }
+});
